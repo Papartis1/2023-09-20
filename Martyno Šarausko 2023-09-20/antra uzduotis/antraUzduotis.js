d34c9bb1d82d13aa54b76e1025a37a25b27d5280
Martynas Šarauskas <75567547+Papartis1@users.noreply.github.com>
2023-09-20 11:25:40 +0300

    Add files via upload

---
+++ Martyno Šarausko 2023-09-20/antra uzduotis/antraUzduotis.js
@@ -0,0 +1,5 @@
+let iveda = +prompt('Įveskite skaičių');
+alert(`Sudėtis: ${iveda} + ${iveda} = ${iveda + iveda}
+Atimtis: ${iveda} - ${iveda} = ${iveda - iveda}
+Daugyba: ${iveda} * ${ivedat} = ${iveda * iveda}
+Dalyba: ${iveda} / ${iveda} = ${iveda / iveda}`);
---
+++ Martyno Šarausko 2023-09-20/pirma uzduotis/pirmaUzduotis.js
@@ -0,0 +1,18 @@
+let A = 'A';
+let B = 's';
+let C = A + B;
+console.log(C);
+
+////////////////////////////////
+
+let pirmasNr = 2;
+let antrasNr = 4;
+let treciasNr = pirmasNr + antrasNr;
+console.log(treciasNr);
+
+////////////////////////////////
+
+let objektasA = { nameA: 'objektas A' };
+let objektasB = { nameB: 'objektas B' };
+let objektasC = objektas.assign(objektasA, objektasB);
+console.log(objektasC);
